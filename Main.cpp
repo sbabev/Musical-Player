b2dbd59f72db03ee0914544b9b2f8f90a9bf1fe8
sbabev <60748876+sbabev@users.noreply.github.com>
2020-12-17 10:36:18 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -15,23 +15,6 @@ int main() {
 
 	HandleInput input;
 	input.play();
-/*
-	Server server;
-	server.writeSongsToFile("Ric-Flair-Drip", "EminemXTyga", "RAP", "RAP2018", 2018 , 1);
-	server.writeSongsToFile("Q-lubq-tebq", "RaufxFalk", "POP", " RusianHits", 2019, 4);
-	server.writeSongsToFile("Adska-pesen" , "Keranov" , "RAP" , "BGRAP" , 2015 , 2);
-		
-	server.writeSongsToFile("All-the-things-she-said" , "Lynhare-Remix" , "ELECTRO" , "CARMUSIC" , 2017 , 9);
-	server.writeSongsToFile("Ayy-Macarena" , "Tyga" , "RAP" , "TYGARAP" , 2018 , 3);
-	server.writeSongsToFile("Izluji-me" , "LidiaXDessita" , "POPFOLK" , "CHALGA2020" , 2019 , 0);
-	server.writeSongsToFile("For-the-night", "LilBabyxDaBaby", "RAP", "RAP2020", 2019, 15);
-*/
-	//server.writeSongsToFile("For-the-night", "LilBaby,DaBaby", "RAP", "RAP2020", 2019 , 5);
-	//server.writeSongsToFile("For-the-night", "LilBaby,DaBaby", "RAP", "RAP2020", 2019, 2);
-	//Adska - pesen  Keranov  RAP  BGRAP  2015  0
-	//	Ayy - Macarena  Tyga  RAP  TYGARAP  2018  0
-	//	Ric - Flair - Drip  EminemXTyga  RAP  RAP2018  2018  0
-		//For - the - night  LilBaby, DaBaby  RAP  RAP2020  2019  0
-//*/
+	
 	return 0;
 }
